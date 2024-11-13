866efc46513098c7081406bf40e065f7e49da575
yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2024-11-13 11:30:08 +0530

    Time: 67 ms (23.19%), Space: 57.3 MB (50.72%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -2,15 +2,47 @@ class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
         long count = 0;
         Arrays.sort(nums);
-        for(int i=0; i<nums.length; i++){
-            int sum = 0;
-            sum = sum + nums[i];
-            for(int j=i+1; j<nums.length; j++){
-                if(sum + nums[j] >= lower && sum + nums[j] <= upper){
-                    count++;
-                }
+        for(int i=0; i<nums.length-1; i++){
+            int left = i+1;
+            int min = minIndex(nums, left, nums.length-1, lower - nums[i]);
+            int max = maxIndex(nums, left, nums.length-1, upper - nums[i]);
+
+            if(min != -1 && max != -1 && min <= max){
+                count = count + max - min +1;
             }
         }
         return count;
+    }
+
+    public int minIndex(int nums[], int left, int right, int sum){
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] >= sum){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        if(left >= nums.length){
+            return -1;
+        }
+        return left;
+    }
+
+    public int maxIndex(int nums[], int left, int right, int sum){
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] <= sum){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        if(right < 0){
+            return -1;
+        }
+        return right;
     }
 }
