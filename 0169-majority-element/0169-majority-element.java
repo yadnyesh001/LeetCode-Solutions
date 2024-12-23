4f107e23931df645803a50491a09c18a87bbf5e0
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2024-12-23 13:19:24 +0530

    Time: 1 ms (99.85%), Space: 53.1 MB (38.75%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int num = 0;
+        int count = 0;
+        for(int i=0; i< nums.length; i++){
+            if(count == 0){
+                num = nums[i];
+            }
+            if(num == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return num;
+    }
+}
