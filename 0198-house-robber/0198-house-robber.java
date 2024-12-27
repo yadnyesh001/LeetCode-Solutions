ff2e33490c9912f37327ec8abad64b297a55953a
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2024-12-28 00:04:56 +0530

    Time: 0 ms (100%), Space: 41.7 MB (5.32%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int curr = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
