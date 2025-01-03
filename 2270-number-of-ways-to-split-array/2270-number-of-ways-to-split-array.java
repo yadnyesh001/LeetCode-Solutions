9d68301e2abb332e927460064920d48d83eff859
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-01-03 12:24:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long sum = 0;
+        int sum = 0;
         for(int i=0; i<nums.length; i++){
             sum = sum + nums[i];
         }
         int count = 0;
-        long newsum = 0;
+        int tillsum = 0;
         for(int i=0; i<nums.length-1; i++){
-            newsum = newsum + nums[i];
-            if(newsum >= sum - newsum){
+            tillsum += nums[i];
+            if(tillsum >= sum - tillsum){
                 count++;
             }
         }
