11743af329e663d4947c99759d8e193f3e898c6b
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-01-29 13:28:57 +0530

    Time: 1 ms (100%), Space: 42.9 MB (22.15%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -19,5 +19,32 @@ class Solution {
 
         if (freshOranges == 0) return 0;
         if (queue.isEmpty()) return -1;
+
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int minutes = -1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            minutes++;
+            
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                
+                for (int[] dir : directions) {
+                    int newRow = current[0] + dir[0];
+                    int newCol = current[1] + dir[1];
+                    
+                    if (newRow >= 0 && newRow < rows && 
+                        newCol >= 0 && newCol < cols && 
+                        grid[newRow][newCol] == 1) {
+                        
+                        grid[newRow][newCol] = 2;  
+                        freshOranges--;
+                        queue.offer(new int[]{newRow, newCol});
+                    }
+                }
+            }
+        }
+        return freshOranges == 0 ? minutes : -1;
     }
 }
