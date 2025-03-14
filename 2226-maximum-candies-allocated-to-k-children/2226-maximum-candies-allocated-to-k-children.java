8b42a8e6c3b5a3af05dc868d39fe21d25b9b3681
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-03-14 13:36:50 +0530

    Time: 23 ms (93.41%), Space: 63.4 MB (22.18%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,28 +1,27 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        int ans=-1;
-        int lo=1, hi=0;
-
-        for(int i=0 ; i<candies.length ; i++){
-            if(candies[i] > hi) hi = candies[i];
-        }
-
-        while(lo <= hi) {
-            int mid = lo + (hi-lo)/2;
-
-            long piles=0;
-            for(int i=0 ; i<candies.length ; i++){
-                piles+=(long)(candies[i]/mid);
-            }
-
-            if(piles >= k) {
-                ans=mid;
-                lo=mid+1;
-            }else{
-                hi=mid-1;
+        int ans = -1;
+        int low = 1;
+        int high = 0;
+        for(int i=0; i<candies.length; i++){
+            if(candies[i] > high){
+                high = candies[i];
             }
         }
-
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            long piles = 0;
+            for(int i=0; i<candies.length; i++){
+                piles += (long)(candies[i]/mid);
+            }
+            if(piles >= k){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
         return ans == -1 ? 0 : ans;
     }
 }
