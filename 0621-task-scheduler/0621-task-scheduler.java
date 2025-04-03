c7b8b9acb471d859d570c24e9ff294bf9fb53dd1
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-04-03 19:41:29 +0530

    Time: 4 ms (73.41%), Space: 46.5 MB (9.34%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,17 +1,16 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int arr[] = new int[26];
+        int[] count = new int[26];
         int max = 0;
-        for(int i=0; i<tasks.length; i++){
-            arr[tasks[i] - 'A']++;
-            max = Math.max(arr[tasks[i] - 'A'], max);
+        int maxCount = 0;
+        for (char c: tasks){
+            count[c-'A']++;
+            max = Math.max(count[c-'A'], max);
         }
-        int count = 0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == max){
-                count++;
-            }
+        for (int i: count){
+            if (i == max)
+                maxCount++;
         }
-        return Math.max(tasks.length, count + (max-1)*(n+1));
+        return Math.max(tasks.length, (max-1) * (n+1) + maxCount);
     }
 }
