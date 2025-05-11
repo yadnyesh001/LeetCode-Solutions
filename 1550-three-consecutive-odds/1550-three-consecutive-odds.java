efd948d0b41d257f267bf6403f914f0912938592
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-05-11 10:37:01 +0530

    Time: 0 ms (100%), Space: 42.1 MB (85.27%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i=1; i<arr.length-1; i++){
+            if(arr[i-1] % 2 == 1){
+                if(arr[i] % 2 == 1){
+                    if(arr[i+1] % 2 == 1){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
