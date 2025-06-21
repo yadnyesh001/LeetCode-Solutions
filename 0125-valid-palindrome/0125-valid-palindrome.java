f559f43cc4e0720957374087b53c8d474d7bad3f
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-06-21 14:06:51 +0530

    Time: 3 ms (72.4%), Space: 43.7 MB (52.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder ans = new StringBuilder();
-
-        // Filter the alphanumeric characters and convert to lowercase
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetterOrDigit(ch)) {
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)){
                 ans.append(Character.toLowerCase(ch));
             }
         }
-
-        // Check if the string is a palindrome
-        String result = ans.toString();
-        for (int i = 0; i < result.length() / 2; i++) {
-            if (result.charAt(i) != result.charAt(result.length() - i - 1)) {
+        String res = ans.toString();
+        for(int i=0; i<res.length()/2; i++){
+            if(res.charAt(i) != res.charAt(res.length()-i-1)){
                 return false;
             }
         }
