65803f0b15903fe0558207e7b0adb1bf83c9a24f
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-06-21 22:32:39 +0530

    Time: 4 ms (100%), Space: 45.5 MB (68.09%) - LeetHub

--- 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.java
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.java
@@ -1,16 +1,25 @@
 class Solution {
     public int minimumDeletions(String word, int k) {
-        int res = 100000, A[] = new int[26];
-        for (char c : word.toCharArray()) {
-            A[c - 'a']++;
+        int[] freq = new int[26];
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            freq[ch - 'a']++;
         }
-        for (int x : A) {
-            int cur = 0;
-            for (int a : A) {
-                cur += (a < x) ? a : Math.max(0, a - (x + k));
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] == 0)
+                continue;
+            int freqEle = freq[i];
+            int max = freqEle + k;
+            int ans = 0;
+            for (int j = 0; j < freq.length; j++) {
+                if (freq[j] < freqEle)
+                    ans = ans + freq[j];
+                else if (freq[j] > max)
+                    ans = ans + freq[j] - max;
             }
-            res = Math.min(res, cur);
+            min = Math.min(min, ans);
         }
-        return res;
+        return min;
     }
 }
