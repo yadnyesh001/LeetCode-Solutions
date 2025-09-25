5aa7bdfaf5f35bcb6be320e1aa655bfd264c055b
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-09-25 09:59:53 +0530

    Time: 8 ms (16.37%), Space: 45.6 MB (30.46%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -3,9 +3,8 @@ class Solution {
         int n = triangle.size();
         for (int row = n - 2; row >= 0; row--) {
             for (int col = 0; col <= row; col++) {
-                triangle.get(row).set(col, triangle.get(row).get(col) + 
-                    Math.min(triangle.get(row + 1).
-                    get(col), triangle.get(row + 1).get(col + 1)));
+                triangle.get(row).set(col, triangle.get(row).get(col) +
+                        Math.min(triangle.get(row + 1).get(col), triangle.get(row + 1).get(col + 1)));
             }
         }
         return triangle.get(0).get(0);
