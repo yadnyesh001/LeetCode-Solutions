82b8deb872f0896a38f2a4f19e025f19fa1df2e8
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-10-18 14:12:40 +0530

    Time: 5 ms (57.82%), Space: 44.8 MB (88.1%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
-        int n = word2.length();
+        int n = word2.length(); 
         int dp[][] = new int[m+1][n+1];
         for(int i=0; i<=m; i++){
-            dp[i][0] = i;
+            dp[i][0] = i; 
         }
         for(int i=0; i<=n; i++){
             dp[0][i] = i;
@@ -15,7 +15,7 @@ class Solution {
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
                 }
             }
         }
