005edcc99c5b929aec561712b497172aafd00ef6
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2025-12-25 12:32:31 +0530

    Time: 5 ms (82.62%), Space: 43.8 MB (84.02%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -2,10 +2,7 @@ class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
         Arrays.sort(arr);
         for(int i=1; i<arr.length-1; i++){
-            if(arr[i] - arr[i-1] == arr[i+1] - arr[i]){
-                continue;
-            }
-            else{
+            if(arr[i] - arr[i-1] != arr[i+1] - arr[i]){
                 return false;
             }
         }
