06ada00c0c3f73e4dc1b828186a194e8a867b1f8
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2026-01-06 18:01:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -1,46 +1,43 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        int max = Integer.MIN_VALUE;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        int curlevel = 1;
-        int maxLevel = 1;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int curSum = 0;
-            for (int i = 1; i <= size; i++) {
-                TreeNode n = q.poll();
-                if (n != null){
-                    curSum += n.val;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int max = 0;
+        int x = 0;
+        int level = 0;
+        while(!queue.isEmpty()){
+            x++;
+            int sum = 0;
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                if(queue.peek().left != null){
+                    queue.add(queue.peek().left);
                 }
-                if (n.left != null){
-                    q.offer(n.left);
+                if(queue.peek().right != null){
+                    queue.add(queue.peek().right);
                 }
-                if (n.right != null){
-                    q.offer(n.right);
-                }
+                sum = sum + queue.poll().val;
             }
-            if (max < curSum) {
-                maxLevel = curlevel;
-                max = curSum;
+            if(sum > max){
+                level = x;
+                max = sum;
             }
-            curlevel++;
         }
-        return maxLevel;
+        return level;
     }
 }
