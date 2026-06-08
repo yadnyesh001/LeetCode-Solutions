098a516e979418f5e051c7009c1e68771a1e52ab
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2026-06-08 13:08:05 +0530

    Time: 4 ms (99.24%), Space: 160.2 MB (26.79%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,28 +1,24 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> list = new ArrayList<>();
-        Stack<Integer> st = new Stack<>();
-        int count = 0;
+        int arr[] = new int[nums.length];
+        int j=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < pivot){
+                arr[j] = nums[i];
+                j++;
+            }
+        }
         for(int i=0; i<nums.length; i++){
             if(nums[i] == pivot){
-                count++;
-            }
-            else if(nums[i] < pivot){
-                st.push(nums[i]);
-            }
-            else{
-                list.add(nums[i]);
+                arr[j] = nums[i];
+                j++;
             }
         }
-        for(int i=0; i<count; i++){
-            list.add(0, pivot);
-        }
-        while(!st.isEmpty()){
-            list.add(0, st.pop());
-        }
-        int arr[] = new int[nums.length];
         for(int i=0; i<nums.length; i++){
-            arr[i] = list.get(i);
+            if(nums[i] > pivot){
+                arr[j] = nums[i];
+                j++;
+            }
         }
         return arr;
     }
