fa196a65ffc5cc7608340b2d4c2f24ad3cfd75ba
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2026-06-17 18:30:06 +0530

    Time: 1 ms (82.36%), Space: 42.8 MB (54.65%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map = new HashMap<>();
+        String arr[] = s.split(" ");
+        if(pattern.length() != arr.length){
+            return false;
+        }
+        for(int i=0; i<pattern.length(); i++){
+            if(!map.containsKey(pattern.charAt(i))){
+                if(map.containsValue(arr[i])) {
+                    return false;
+                }
+                map.put(pattern.charAt(i), arr[i]);
+            }
+            else{
+                if(!map.get(pattern.charAt(i)).equals(arr[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
