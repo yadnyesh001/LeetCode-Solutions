0e37b5814f32e31cbbf92c8349f702233d44afb2
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2026-06-21 11:00:32 +0530

    Time: 37 ms (56.85%), Space: 83.7 MB (68.89%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count = 0;
+        for(int i=0; i<costs.length; i++){
+            if(coins - costs[i] >= 0){
+                count++;
+                coins = coins - costs[i];
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
