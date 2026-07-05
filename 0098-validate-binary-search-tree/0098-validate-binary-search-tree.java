d34e08c0bb11a52f7f74a9c67216925d6c9840f9
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2026-07-05 11:40:43 +0530

    Time: 1 ms (16.67%), Space: 45.7 MB (7.45%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,32 +1,37 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        int n = list.size();
+        for(int i=0; i<n-1; i++){
+            if(list.get(i) >= list.get(i+1)){
+                return false;
+            }
+        }
+        return true;
     }
-    
-    private boolean isValidBST(TreeNode node, Integer min, Integer max) {
-        if (node == null) {
-            return true;
+
+    public void inorder(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
         }
-        if ((min != null && node.val <= min) || 
-        (max != null && node.val >= max)) {
-            return false;
-        }
-        return isValidBST(node.left, min, node.val) && 
-        isValidBST(node.right, node.val, max);
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
     }
 }
