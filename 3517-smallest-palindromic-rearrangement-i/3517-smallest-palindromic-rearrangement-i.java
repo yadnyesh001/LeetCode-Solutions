ae657c4109580a38f8a37ce3f1adc8cbb5c05d45
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2026-07-28 11:02:53 +0530

    Time: 31 ms (63.05%), Space: 48.1 MB (57.43%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,21 +1,24 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int[] arr = new int[26];
-        for (char c : s.toCharArray()) {
-            arr[c - 'a']++;
+        int arr[] = new int[26];
+        for (char ch : s.toCharArray()) {
+            arr[ch - 'a']++;
         }
-        StringBuilder left = new StringBuilder();
-        String middle = "";
+        StringBuilder first = new StringBuilder();
         for (int i = 0; i < 26; i++) {
-            if (arr[i] % 2 == 1) {
-                middle = String.valueOf((char) (i + 'a'));
+            for (int j = 0; j < arr[i] / 2; j++) {
+                first.append((char)(i + 'a'));
             }
-            int count = arr[i] / 2;
-            for (int j = 0; j < count; j++) {
-                left.append((char) (i + 'a'));
+            arr[i] %= 2;   
+        }
+        StringBuilder ans = new StringBuilder(first);
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == 1) {
+                ans.append((char)(i + 'a'));
+                break;
             }
         }
-        String right = new StringBuilder(left).reverse().toString();
-        return left.toString() + middle + right;
+        ans.append(first.reverse());
+        return ans.toString();
     }
 }
